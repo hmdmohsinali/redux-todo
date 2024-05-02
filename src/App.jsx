889e8017842b5ec5e396164edf76605498ff8ebf
Mohsin A. <hmdmohsinali@gmail.com>
2024-05-02 15:58:22 +0500

    completed: delete functionality

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import AddTodo from './components/AddTodo'
+import './App.css'
+import Todos from './components/Todos';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>learn redux</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todos.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import { removeTodo } from "../features/todo/todoSlice";
+
+const Todos = () => {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded "
+          >
+            <div className="text-white">{todo.text}</div>
+            <button
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-base"
+              onClick={() => dispatch(removeTodo(todo.id))}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="w-6 h-6"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"
+                ></path>
+              </svg>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Todos;
